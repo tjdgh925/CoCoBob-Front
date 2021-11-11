7031fa44292956bf9b48ea79760f9e352418da51
장성호 <tjdgh925@gmail.com>
2021-11-11 23:07:33 +0900

    feat: 로그인 관련 Redux Slice 생성 (#4)

--- cocobob/src/features/auth/slices.tsx
+++ cocobob/src/features/auth/slices.tsx
@@ -1,1 +1,70 @@
-export {};
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AxiosError } from 'axios';
+import {
+  loginData,
+  loginState,
+  signUpData,
+  signUpState,
+} from '../../types/types';
+
+const initialState: loginState | signUpState = {
+  error: {
+    loading: false,
+    error: null,
+  },
+  auth: null,
+  data: null,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login(state, action: PayloadAction<loginData>) {
+      state.error.loading = true;
+      state.data = action.payload;
+    },
+    loginSuccess(state, action: PayloadAction<boolean>) {
+      state.error.loading = false;
+      state.auth = action.payload;
+    },
+    loginFailure(state, action: PayloadAction<AxiosError>) {
+      state.error.loading = false;
+      state.error.error = action.payload;
+      state.auth = false;
+    },
+    tempSetUser(state, action: PayloadAction<loginData>) {
+      state.error.loading = true;
+      state.data = action.payload;
+    },
+    check(state) {
+      state.error.loading = true;
+    },
+    checkSuccess(state, action: PayloadAction<boolean>) {
+      state.error.loading = false;
+      state.auth = action.payload;
+    },
+    checkFailure(state, action: PayloadAction<AxiosError>) {
+      state.error.loading = false;
+      state.error.error = action.payload;
+      state.auth = false;
+    },
+    logout(state) {
+      state.error.loading = false;
+      state.auth = false;
+    },
+  },
+});
+
+export const {
+  login,
+  loginSuccess,
+  loginFailure,
+  tempSetUser,
+  check,
+  checkSuccess,
+  checkFailure,
+  logout,
+} = authSlice.actions;
+
+export default authSlice.reducer;
