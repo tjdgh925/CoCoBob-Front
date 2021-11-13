2aebb6188d3dee5fcba1da4b2ca7718aa8ac2b25
장성호 <tjdgh925@gmail.com>
2021-11-14 02:12:19 +0900

    feat: 회원가입 페이지 UI 구현

--- cocobob/src/pages/auth/RegisterPage.tsx
+++ cocobob/src/pages/auth/RegisterPage.tsx
@@ -1,5 +1,96 @@
+import { useState, useCallback } from 'react';
+
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+import Header from '../../components/common/Header';
+import RegisterHeader from '../../components/auth/Register/RegisterHeader';
+import RegisterForm from '../../components/auth/Register/RegisterForm';
+import RegisterButton from '../../components/auth/Register/RegisterButton';
+import { signUpData } from '../../types/types';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      textAlign: 'center',
+    },
+    card: {
+      marginTop: theme.spacing(10),
+      paddingTop: theme.spacing(5),
+      paddingBottom: theme.spacing(5),
+      border: '1px solid black',
+      boxShadow: 'none',
+    },
+  })
+);
+
 const RegisterPage = () => {
-  return <div>회원가입</div>;
+  const classes = useStyles();
+
+  const [birthday, setBirthday] = useState<Date | null>(new Date());
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+  const [signUpInfo, setSignUpInfo] = useState<signUpData>({
+    email: '',
+    user_pw: '',
+    sex: '',
+    birth: '',
+  });
+
+  const checkPassword = (passwordConfirm: string): boolean => {
+    if (passwordConfirm === signUpInfo.user_pw) return false;
+    else return true;
+  };
+
+  const onSubmit = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (birthday instanceof Date)
+        setSignUpInfo({
+          ...signUpInfo,
+          birth: birthday.toISOString().slice(0, 10).replace(/-/g, ''),
+        });
+      // dispatch(register(signUpInfo));
+    },
+    [birthday]
+  );
+
+  const onChange = useCallback(
+    (e) => {
+      console.log(e.target);
+      const { name, value } = e.target;
+      if (name === 'passwordConfirm') {
+        setPasswordConfirm(value);
+      } else
+        setSignUpInfo({
+          ...signUpInfo,
+          [name]: value,
+        });
+    },
+    [signUpInfo]
+  );
+
+  return (
+    <Container className={classes.container}>
+      <Header />
+      <Card className={classes.card} variant={'outlined'}>
+        <RegisterHeader />
+        <Container>
+          <RegisterForm
+            signUpInfo={signUpInfo}
+            passwordConfirm={passwordConfirm}
+            onChange={onChange}
+            checkPassword={checkPassword}
+            birthday={birthday}
+            setBirthday={setBirthday}
+          />
+          {/* {error.error?.message !== undefined && (
+            <ErrorMessage>{'회 원 가 입  실 패 !'}</ErrorMessage>
+          )} */}
+          <RegisterButton onSubmit={onSubmit} />
+        </Container>
+      </Card>
+    </Container>
+  );
 };
 
 export default RegisterPage;
