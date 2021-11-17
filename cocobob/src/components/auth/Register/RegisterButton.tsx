161e0939873bb0af9b8d23dbbd4058df67aaad4b
장성호 <tjdgh925@gmail.com>
2021-11-17 13:37:09 +0900

    feat: HeaderNavigationBar 구현 (#11)

--- cocobob/src/components/auth/Register/RegisterButton.tsx
+++ cocobob/src/components/auth/Register/RegisterButton.tsx
@@ -1,38 +1,11 @@
-import styled, { css } from 'styled-components';
+import Button from '../../common/Button';
 
 interface RegisterButtonProps {
   onSubmit: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const buttonStyle = css<{ color?: string }>`
-  border: none;
-  border-radius: 8px;
-  font-size: 1rem;
-  font-weight: bold;
-  color: white;
-  outline: none;
-  cursor: pointer;
-  padding-top: 0.75rem;
-  padding-bottom: 0.75rem;
-  width: 100%;
-  font-size: 1.125rem;
-  background: ${(props) => (props.color ? props.color : 'gray')};
-  &:hover {
-    background: black;
-  }
-`;
-
-const StyledButton = styled.button`
-  ${buttonStyle}
-  margin-bottom: .8rem;
-`;
-
 const RegisterButton = ({ onSubmit }: RegisterButtonProps) => {
-  return (
-    <StyledButton onClick={onSubmit} form="register">
-      회원가입
-    </StyledButton>
-  );
+  return <Button onClick={onSubmit}>회원가입</Button>;
 };
 
 export default RegisterButton;
