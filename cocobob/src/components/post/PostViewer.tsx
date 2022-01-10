6fd258c8859f3f8f96234e51591b94c807b1aed9
장성호 <tjdgh925@gmail.com>
2022-01-10 18:43:28 +0900

    feat: 댓글 작성 부분 UI 구현 (#30)

--- cocobob/src/components/post/PostViewer.tsx
+++ cocobob/src/components/post/PostViewer.tsx
@@ -1,17 +1,33 @@
 import styled from 'styled-components';
+import CommentWrite from './CommentWrite';
 import ContentBody from './ContentBody';
 import ContentSideView from './ContentSideView';
 
 const PostContentBlock = styled.div`
+  flex-direction: column;
   display: flex;
-  height: 800px;
+  height: 1000px;
+`;
+const ContentBlock = styled.div`
+  width: 100%;
+  display: flex;
+  height: 80%;
+`;
+const CommentBlock = styled.div`
+  width: 100%;
+  display: flex;
 `;
 
 const PostViewer = () => {
   return (
     <PostContentBlock>
-      <ContentBody />
-      <ContentSideView />
+      <ContentBlock>
+        <ContentBody />
+        <ContentSideView />
+      </ContentBlock>
+      <CommentBlock>
+        <CommentWrite />
+      </CommentBlock>
     </PostContentBlock>
   );
 };
