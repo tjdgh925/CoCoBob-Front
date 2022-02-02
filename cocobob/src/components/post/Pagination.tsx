09b2110528dc1d6c47c8406e9f76427d0126a96c
장성호 <tjdgh925@gmail.com>
2022-02-02 15:21:09 +0900

    feat: Pagination UI 수정 (#38)

--- cocobob/src/components/post/Pagination.tsx
+++ cocobob/src/components/post/Pagination.tsx
@@ -1,16 +1,35 @@
 import qs from 'qs';
 import styled from 'styled-components';
 import { withRouter, useLocation, Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
 
 const PaginationBlock = styled.div`
-  width: 90%;
+  width: 100%;
   height: 2rem;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-self: center;
 `;
 
-const Button = styled.button``;
+const PaginationButton = styled.button<{ edge?: boolean }>`
+  background: none;
+  border: 1px solid ${palette.gray[0]};
+  width: 4rem;
+  height: 2.5rem;
+  font-size: 1rem;
+  :disabled {
+    color: white;
+    background-color: ${palette.gray[0]};
+  }
+  ${(props) =>
+    props.edge
+      ? `width: 6rem;
+        :disabled{
+            color: ${palette.gray[0]};
+            background: none;
+        }`
+      : ``};
+`;
 
 const index: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const Pagination = () => {
@@ -21,17 +40,26 @@ const Pagination = () => {
   return page && typeof page === 'string' ? (
     <PaginationBlock>
       <Link to={`/post?page=${parseInt(page, 10) - 1}`}>
-        <Button disabled={page === '0'}> 이전 </Button>
+        <PaginationButton edge disabled={page === '0'}>
+          {' '}
+          ◀ PREV{' '}
+        </PaginationButton>
       </Link>
       {index.map((idx) => {
         return (
           <Link to={`/post?page=${idx}`}>
-            <Button disabled={page === idx}> {parseInt(idx, 10) + 1} </Button>
+            <PaginationButton disabled={page === idx}>
+              {' '}
+              {parseInt(idx, 10) + 1}{' '}
+            </PaginationButton>
           </Link>
         );
       })}
       <Link to={`/post?page=${parseInt(page, 10) + 1}`}>
-        <Button disabled={page === '9'}> 다음 </Button>
+        <PaginationButton edge disabled={page === '9'}>
+          {' '}
+          NEXT ▶
+        </PaginationButton>
       </Link>
     </PaginationBlock>
   ) : (
